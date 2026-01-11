d425c45866c3009ef331f3a4d34f59ad7da31ea9
Nikhil Kumar <141623909+nikhilkumar7056@users.noreply.github.com>
2026-01-11 17:43:50 +0000

    hekkeiands

---
+++ control_Flow/IF.js
@@ -0,0 +1,47 @@
+// if condition statement
+
+// if(condition){ condition is true then if statement is execute
+    
+// }
+
+// condition are
+
+// <,>,<=,>=,==,===
+ 
+// const tempurater = 41
+
+// if(tempurater == 50){
+//     console.log("tempurater is less than 50")
+// } else{
+//     console.log("greater than 50")
+// }
+
+// console.log("tempurater is greater than 50");
+
+// const balance =1000
+// if(balance > 500) console.log("balance is greater than 500"),console.log("balance is 1000");
+
+// nest condition_________________________________________
+
+// const balance = 1000
+
+// if (balance <500) {
+//     console.log("balance is less than 500")
+// } else if(balance <750){
+//     console.log("balance is less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900")
+// } else {
+//     console.log("less than 1200")
+// }
+
+// const loggedIN = true
+// const loggedOut = false
+
+// if(loggedIN && loggedOut){  both condition are true than execute
+//     console.log("user is loggedIN in the site")
+// }
+
+// if(loggedIN || loggedOut){ only one condition will execute
+//     console.log("user may be loggedIN")
+// }
